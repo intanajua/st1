c1c74b4befaebc6b96655a1f7c22d00e9c83850a
intanajua <113221557+intanajua@users.noreply.github.com>
2022-09-10 10:18:08 +0800

    Add files via upload

---
+++ 9-app1.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+# title
+
+
+# height
+
+
+# weight
+
+
+# calculate bmi
+
+
+# message alert
+# follow classifation below
+# http://www.myhealth.gov.my/indeks-jisim-tubuh-ijt/
+
+
+
+
+
+
+# caption
